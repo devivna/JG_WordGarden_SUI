a25840906d4b5c20885fb2e4c74299c28aca8ef5
Nadia <ivashchenko.nadia@gmail.com>
2023-02-01 20:08:29 +0200

    2.3 work with TextField and Button

--- WordGarden-SwiftUI/ContentView.swift
+++ WordGarden-SwiftUI/ContentView.swift
@@ -8,14 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT", "CAT", "DOG"]
+    
+    @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
+    @State private var guessedLetter = ""
+    @State private var imageName = "sun.max"
+    @State private var playAgainHidden = true
+    
+    @FocusState private var textFieldIsFocus: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Words to Guess: \(wordsToGuess.count - (wordsGuessed + wordsMissed))")
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }
+            }
+            .padding(.horizontal)
+            
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // TODO: Switch to wordsToGuess[currentWord]
+            Text("_ _ _ _")
+                .font(.title)
+            
+            if playAgainHidden {
+                HStack {
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                    //2.3
+                        .keyboardType(.alphabet)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) { _ in
+                            guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+                        
+                            guard let lastChar = guessedLetter.last else {
+                                return
+                            }
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFieldIsFocus)
+                        
+                    
+                    
+                    Button {
+                        textFieldIsFocus = false
+                    } label: {
+                        Text("Guess a letter")
+                    }
+                    .buttonStyle(.bordered)
+                    .disabled(guessedLetter.isEmpty)
+                    
+                }
+            } else {
+                
+                Button {
+                    //
+                } label: {
+                    Text("Another word?")
+                }
+                .buttonStyle(.borderedProminent)
+
+            }
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300)
+        
         }
-        .padding()
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
@@ -24,3 +109,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
